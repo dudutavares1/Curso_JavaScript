95cc080652ad3cfe902d04b3f878cecb71e09e6c
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-06-10 18:46:41 -0300

    condições
    aula 2

---
+++ Condições JS/ex003.js
@@ -0,0 +1,6 @@
+var vel = 100
+console.log(` a velocidade di seu carro é de ${vel} Km/h`)
+if (vel > 60){
+    console.log(` voce foi multado`)
+}
+console.log(` digija sempre usando o cinto de segunraçah`)
---
+++ Condições JS/ex004.js
@@ -0,0 +1,8 @@
+var pais = 'brasil'
+console.log (`viendo em ${pais}`)
+if (pais != "brasil"){
+    console.log('brasileiro')
+
+}else {
+    console.log('estrangeiro')
+}
