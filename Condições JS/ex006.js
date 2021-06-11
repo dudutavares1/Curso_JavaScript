d5e2c0fc2d727d7b97de92b8d6307cfc59989ce8
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-06-10 21:40:34 -0300

    swtich
    swtich

--- Condições JS/ex006.js
+++ Condições JS/ex006.js
@@ -1,4 +1,14 @@
-var idade = 22
+var idade = 50
 if (idade < 18){
-    console.log()
+    console.log('nao vota')
+} 
+else if (idade < 18 || idade > 65){
+       
+    console.log('Voto Opcional')
+    
+}else {
+    console.log('Voto Obrigatorio')
 }
+
+
+
