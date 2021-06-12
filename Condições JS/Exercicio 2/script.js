130ca8d2f4d72c9add082222e735ea98d1b7b64f
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-06-12 14:28:08 -0300

    Update script.js

--- Condições JS/Exercicio 2/script.js
+++ Condições JS/Exercicio 2/script.js
@@ -5,6 +5,47 @@ var data = new Date ()
 var ano = data.getFullYear()
 var fano = document.getElementById('txtano')
 var res = document.querySelector('div#res')
-if (fano.nodeValue.length == 0 )
+if (fano.value.length == 0 || fano.value > ano){
+
+    window.alert ('[ERRO] verifique od das e tente novamente !')
+
+}else {
+    var fsex = document.getElementsByTagName('radsex')
+    var idade = ano - Number (fano.value)
+    var genero = ''
+    if (fsex [0].checked){
+        genero = 'Homem'
+        if(idade >= 0 && idade <10){
+            //crianca
+        }else if (idade < 21){
+            //jovem
+        } else if (idade < 50){
+        //adulto
+    }else {
+        //idoso
+    }
+
+
+    }else if (fsex [1].checked){
+        genero = 'mulher'
+        if(idade >= 0 && idade <10){
+            //crianca
+        }else if (idade < 21){
+            //jovem
+        } else if (idade < 50){
+        //adulto
+    }else {
+        //idoso
+    }
+
+
+    }
+
+res.getElementsByClassName.texAling = 'center'
+res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+}
+
+
+
 
 }
