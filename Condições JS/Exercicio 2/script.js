9330c2fe7a5e6eac7360e603216c3fd2d17e64fa
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-06-12 14:00:33 -0300

    atualizando fotos
    atualizando fotos

--- Condições JS/Exercicio 2/script.js
+++ Condições JS/Exercicio 2/script.js
@@ -1,31 +1,10 @@
-function carregar() {
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('imagem')
+function verificar() {
+
+alert('funcionou')
 var data = new Date ()
-var hora = data.getHours ()
-msg.innerText = `Agora são ${hora} Horas.`
-
-if(hora >= 0 && hora < 12){
-    //bom dia 
-img.src = '../Modelo/manha.png'
-document.body.style.background = '#e2cd9f'
-
-}
-else if (hora >= 12 && hora < 18){
-    //boa tarde
-    img.src = '../Modelo/tarde.png'
-    document.body.style.background = '#b9846f'
-
-
-} else {
-    // boa noite
-    img.src = '../Modelo/noite.png'
-    document.body.style.background = '#515154'
-
-
-
-}
-
-
+var ano = data.getFullYear()
+var fano = document.getElementById('txtano')
+var res = document.querySelector('div#res')
+if (fano.nodeValue.length == 0 )
 
 }
