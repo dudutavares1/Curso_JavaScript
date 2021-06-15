fc15cc9c24bf0e0b651bec3a9ee1cd895481e562
Oficina Caçapava 350 <oficicacapava350@outlook.com>
2021-06-15 15:53:31 -0300

    ultimo exercicio do cusrtos

---
+++ Repetições/Variáveis Compostas/ex001.js
@@ -0,0 +1,12 @@
+let valores = [8, 1, 7, 4, 2, 9 ]
+valores.sort()   // organiza por ordem numerica ou alfabetica 
+num.indexOf (7) // retorna a chave do vetor e mostra a sua posição
+for(let pos in valores){
+    console.log(` A posição ${pos} tem o valor ${valor[pos]}`)
+
+
+/*
+ 
+ 
+
+}
---
+++ Repetições/Variáveis Compostas/ex002.js
@@ -0,0 +1,10 @@
+function parimpar(n) {
+    if (n % 2 == 0){
+        return 'par'
+    }else {
+        return 'impar'
+    }
+}
+
+l
+console.log(parimpar(223))
