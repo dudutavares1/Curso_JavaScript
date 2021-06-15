53bfc2253b7ff8668c9bb3939dbcb39206032ded
Oficina Caçapava 350 <oficicacapava350@outlook.com>
2021-06-15 11:48:16 -0300

    lços

--- Condições JS/Exercicio 2/script.js
+++ Condições JS/Exercicio 2/script.js
@@ -5,6 +5,9 @@ var data = new Date ()
 var ano = data.getFullYear()
 var fano = document.getElementById('txtano')
 var res = document.querySelector('div#res')
+
+
+
 if (fano.value.length == 0 || fano.value > ano){
 
     window.alert ('[ERRO] verifique od das e tente novamente !')
@@ -13,6 +16,9 @@ if (fano.value.length == 0 || fano.value > ano){
     var fsex = document.getElementsByTagName('radsex')
     var idade = ano - Number (fano.value)
     var genero = ''
+    var img = document.createElement ('img')
+    img.setAttribute ('id', 'foto')
+
     if (fsex [0].checked){
         genero = 'Homem'
         if(idade >= 0 && idade <10){
