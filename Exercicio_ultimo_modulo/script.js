cddad10d6a319d00b6bdb0e356c9c4772d4c3865
dudutavares <80931058+dudutavares@users.noreply.github.com>
2021-06-15 18:22:37 -0300

    fim dos exercicios
    fim dos exercicios

--- Exercicio_ultimo_modulo/script.js
+++ Exercicio_ultimo_modulo/script.js
@@ -1,36 +1,36 @@
-function tabuada(){
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
 
-    if (num.value.length == 0){
-        window.alert('digite um numero') 
-        
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+
+ function isNumero(n){
+     if (Number(n) >= 1 && Number(n) <= 100){
+         return true
+    } else { 
+        return false
+ }
+}
+
+
+function  inLista (n, l) {
+    if (l.indexOf(Number(n)) !n -1) {
+        return true
     }else {
-        let n = Number(num.value)
-        let c = 1 
-        tab.innerHTML = ''
-        while (c <= 10){
-            let item = document.createElement('option')
-            item.text = `${n} X ${c} = ${n*c} `
-            item.value = `tab${c}`
-            tab. appendChild(item)
-            c++
-        }
-
-
+        return false
     }
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)){
+       valores.push(Number(num.value))
+       let item = document.createElement('option')
+       item.text = `Valor ${num.value} Adicionado`
+       lista.appendChild(item)
+    }else {
+        window.alert('Valor Invalido')
+    }
 }
